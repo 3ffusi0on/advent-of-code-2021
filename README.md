47c4f9fa9ffe28485c716a0630a5c17f062cbc94
Pierre-Alain SIMON <3ffusi0on@gmail.com>
2021-12-10 09:30:30 +0100

    template folder for next days

--- README.md
+++ README.md
@@ -29,4 +29,3 @@ My solutions to the [Advent of Code 2021](https://adventofcode.com/2021) problem
 | [Day 23](./src/day23/)                  | ...        | 🕒🕒 |
 | [Day 24](./src/day24/)                  | ...        | 🕒🕒 |
 | [Day 25](./src/day25/)                  | ...        | 🕒🕒 |
-| --------------------------------------- | ---------- | ---- |
---
+++ src/dayXX/solutions.js
@@ -0,0 +1,36 @@
+const { log } = require('console');
+const fs = require('fs')
+
+function getInput() {
+  var input = [];
+  try {
+    var data = fs.readFileSync('./resources/dayXX/input.txt', 'UTF-8').split(/\r?\n/);
+    data = fs.readFileSync('./resources/dayXX/input-test.txt', 'UTF-8').split(/\r?\n/);
+    data.forEach((line) => {
+      if (line.length > 0) {
+        input.push(line.split(""));
+      }
+    });
+  } catch (err) {
+    console.error(err);
+  }
+  return input;
+}
+
+
+function solve1() {
+  var input = getInput();
+  var result = 0;
+
+  return result;
+}
+
+function solve2() {
+  var input = getInput();
+  var result = 0;
+
+  return result;
+}
+
+console.log(solve1())
+console.log(solve2())
