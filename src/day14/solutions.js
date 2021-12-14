f3f8f8c9dd01a3dba546c81176bbe97dd8750ec0
Pierre-Alain SIMON <3ffusi0on@gmail.com>
2021-12-14 16:08:11 +0100

    WIP-day14-part2

--- src/day14/solutions.js
+++ src/day14/solutions.js
@@ -60,10 +60,51 @@ function solve1() {
 
 function solve2() {
   var input = getInput();
-  var result = 0;
+  var count = new Map();
+  var chunkSize = 19922945;
+  var chunks = [];
 
-  return result;
+  var template = input.polymer;
+  for (let s = 0; s < 40; s++) {
+    console.log(`chunking ${s}`, template.length);
+    if (template.length > chunkSize) {
+      chunks.push(template.substring(0, chunkSize));
+      chunks.push(template.substring(chunkSize - 1)); // last char of 1st chunk is included
+    }
+
+    //TODO finish chinking algorithm
+
+    var replaced = "";
+    for (let i = 0; i < template.length; i++) {
+      let leftover = template.substring(i);
+      let insertion = input.insertions.get(leftover[0] + leftover[1]);
+      if (insertion !== undefined) {
+        replaced = `${replaced}${leftover[0]}${insertion}`;
+      } else {
+        replaced = `${replaced}${leftover[0]}`;
+      }
+    }
+    template = replaced;
+  }
+
+
+  for (let i = 0; i < template.length; i++) {
+    let char = template[i];
+    if (count.has(char)) {
+      count.set(char, count.get(char) + 1);
+    } else {
+      count.set(char, 1);
+    }
+  }
+
+
+
+
+  var max = [...count].reduce((a, b) => a[1] > b[1] ? a : b);
+  var min = [...count].reduce((a, b) => a[1] < b[1] ? a : b);
+
+  return max[1] - min[1];
 }
 
-console.log(solve1())
-// console.log(solve2())
+// console.log(solve1())
+console.log(solve2())
